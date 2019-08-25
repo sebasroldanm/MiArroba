ad8d3b30dff027a44502878ca2844373095ae3f6
Sebastian Roldan <sebasroldanm@gmail.com>
2019-08-25 13:54:26 -0500

    Conexión Server Producción

--- conex.php
+++ conex.php
@@ -1,8 +1,13 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pw = "";
+// $host = "localhost";
+// $user = "root";
+// $pw = "";
+// $bd = "yohanroldan";
+
+$host = "mysql.webcindario.com";
+$user = "yohanroldan";
+$pw = "xevitax2012";
 $bd = "yohanroldan";
 
 ?>
